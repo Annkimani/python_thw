dbc41e1859168761b8a5ab9126e1c9d5a8ce6691
Annkimani <annkimani270@gmail.com>
2018-08-05 19:03:07 +0300

    add math functions

---
+++ function_example.py
@@ -0,0 +1,15 @@
+# What is a function
+# A function is a block of organized, reusable code that is used to perform a single, related action
+print("hello there")
+def multiply(num_one,  num_two):
+    results = num_one * num_two
+    return results
+
+def division(num_one, num_two):
+    results = num_one / num_two
+    return results
+
+
+print(multiply(540, 50))
+print(division(2500, 50))
+print(division(240, 3))
