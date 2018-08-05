df6378bee51ce6899aac9ff306ac7c76ccb53bf3
Annkimani <annkimani270@gmail.com>
2018-08-05 19:04:22 +0300

    add variables

---
+++ maths_multiplication.py
@@ -0,0 +1,17 @@
+my_variable = 1
+number_one  = 2
+print(my_variable + number_one - number_one)
+
+first_name = "ann"
+second_name = "obaga"
+print(first_name + second_name)
+
+name_list = ["ann", "beth", "gidraff"]
+print(name_list[2])
+
+num_one = 1
+num_two = str(num_one)
+print(type(num_one))
+print(num_two)
+print(type(num_two))
+
