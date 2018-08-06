373cab44ef40ec58afa6d468aae93152892d3a0e
Annkimani <annkimani270@gmail.com>
2018-08-06 13:09:30 +0300

    data_structures.py

---
+++ data_structures.py
@@ -0,0 +1,17 @@
+friends = ["evans", "beth", "gidraff"]
+friends.append("ann")
+print(friends)
+friends.insert("keziah")
+print(friends)
+
+fruits = {"fruit_one":"watermeon","fruit_two":"grapes", "fruit_three":"apple"}
+print(fruits["fruit_one"])
+
+
+counties = {1:"mombasa", 2:"nairobi", 3:"kiambu"}
+print(counties[2])
+
+
+
+number = (3, 3, 2, 2)
+print(number)
